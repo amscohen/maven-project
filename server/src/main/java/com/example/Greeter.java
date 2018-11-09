bab734b75f594bc5fd77c180344916bb3c3e1457
ac <ac@jenkins-master.mshome.net>
2018-11-09 11:38:10 -0500

    throws Exception

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,16 +12,16 @@ public class Greeter {
 
   }
 
-  public String greet(String someone) {
+  public String greet(String someone) throws Exception {
     return greet(someone, 0);
   }
 
   //TODO: Add javadoc comment
-  public String greet(String someone, int iterations) {
+  public String greet(String someone, int iterations) throws Exception {
     for (int i=0;i<iterations;i++){
         int sleepMS = i*i;
         System.out.println("Iteration " + i + ". Sleeping for " + sleepMS + " MS.");
-        Thread.sleep(slepMS);
+        Thread.sleep(sleepMS);
     }
     return String.format("Hello, %s!", someone);
   }
